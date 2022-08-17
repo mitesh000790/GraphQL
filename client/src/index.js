959676c54fc71c12418bf978ea60bd22e1c97b5b
mitesh000790 <mitesh000790@gmail.com>
2022-08-17 17:31:56 +0530

    add pwa service worker

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import {
     ApolloProvider,
 } from "@apollo/client";
 import { ThemeProvider } from "./component/themeContext"
+import swDev from "./swDev";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -37,4 +38,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+swDev()
