634683607d7d3545571f064760839fa76b6acfc0
mitesh000790 <mitesh000790@gmail.com>
2022-08-22 15:58:55 +0530

    remove react-helmet

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,11 @@ import './App.css';
 import React from "react";
 import Navbar from "./containers/Navbar/Navbar";
 import Routing from "./containers/Route/Routes";
-import Helmet from "react-helmet"
 
 function App() {
     const online = navigator.onLine;
   return (
     <div>
-        {/*<Helmet*/}
-        {/*    titleTemplate="%s - An ....."*/}
-        {/*    defaultTitle="Quote Book"*/}
-        {/*>*/}
-        {/*    <meta name="description" content="Quote Book" />*/}
-        {/*</Helmet>*/}
         <Navbar/>
         {online === false && <div className="bg-red-100 w-full h-10 p-2 fixed mt-10 text-center  px-6 text-base text-red-700 dark:bg-red-300 dark:text-white" style={{marginTop: "70px"}} role="alert">
             You are in offline mode or some issue with connection.
